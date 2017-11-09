6f1925849e43e056083c41ca6efde496cb448b27
satishvattikuti <satishvattikuti94@gmail.com>
2017-11-08 19:19:36 -0500

    backend up and running

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/wishme', (req, res) => {
+    res.send('First version of back end');
+});
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
