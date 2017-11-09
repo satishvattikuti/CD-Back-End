7538354a0ebdb54176b4fab3b49f7f04840f7269
satishvattikuti <satishvattikuti94@gmail.com>
2017-11-08 19:27:11 -0500

    route Changed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 
-app.get('/wishme', (req, res) => {
+app.get('/hello', (req, res) => {
     res.send('First version of back end');
 });
 
